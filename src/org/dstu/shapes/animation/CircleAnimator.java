61a81a893f89058a3f7c9ebd281b183c83d7ccbc
Artyom Gornostal <nunuhi721@gmail.com>
2023-05-28 14:47:28 +0300

    create circle animation to move by borders and change colors when change direction

--- src/org/dstu/shapes/animation/CircleAnimator.java
+++ src/org/dstu/shapes/animation/CircleAnimator.java
@@ -4,6 +4,8 @@ import org.dstu.shapes.Circle;
 import org.dstu.shapes.Shape;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
 public class CircleAnimator extends Animator {
 
@@ -11,15 +13,52 @@ public class CircleAnimator extends Animator {
         super(shape, component);
     }
 
+    private void assignRandomColor() {
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float g = rand.nextFloat();
+        float b = rand.nextFloat();
+        shape.setColor(new Color(r, g, b));
+    }
     @Override
     public void run() {
+        int radius = ((Circle) shape).getRadius();
+        int step = 10;
         while (true) {
-            shape.moveRel((int) (Math.random() * 10), (int) (Math.random() * 10));
+            int screenWidth = component.getWidth();
+            int screenHeight = component.getHeight();
+
+            if (shape.getY() == 0 && shape.getX() < (screenWidth - radius)) {
+                shape.moveRel(step, 0);
+            }
+
+            if (shape.getX() == (screenWidth - radius) && shape.getY() < (screenHeight - radius)) {
+                shape.moveRel(0, step);
+            }
+
+            if (shape.getY() == (screenHeight - radius) && shape.getX() > 0) {
+                shape.moveRel(-step, 0);
+            }
+
+            if (shape.getX() == 0 && shape.getY() > 0) {
+                shape.moveRel(0, -step);
+            }
+
+            if (
+                    shape.getY() == 0 && shape.getX() == (screenWidth - radius - step)
+                            || shape.getX() == (screenWidth - radius) && shape.getY() == (screenWidth - radius - step)
+                            || shape.getY() == (screenHeight - radius - step) && shape.getX() == 0
+                            || shape.getY() == 0 && shape.getX() == 0
+            ) {
+                assignRandomColor();
+            }
+
+
             if (component != null) {
                 component.repaint();
             }
             try {
-                Thread.sleep(250);
+                Thread.sleep(90);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
